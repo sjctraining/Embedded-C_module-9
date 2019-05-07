d8b1455498675c5af0c245854a2a3c24e23a413d
Subitha <subithamohanps@gmail.com>
2019-05-07 22:04:16 +0530

    fixed intendation

--- LCD.c
+++ LCD.c
@@ -11,47 +11,42 @@ void delay();
 
 void main()
 {   
-	  LCD_Cmd(0x38);  // set as dot matrix 5x7
-	  LCD_Cmd(0x01);  // clear screen
-		LCD_Cmd(0x0F);  // Display on cursor blinking
-		     
-    LCD_Data('C'); 		       
-		LCD_Data('O');		        	
-    LCD_Data('M');		       
-    LCD_Data('O');		       					
-		LCD_Data('R');		      
-		LCD_Data('I');		      
-		LCD_Data('N');
-	  
+    LCD_Cmd(0x38);  // set as dot matrix 5x7
+    LCD_Cmd(0x01);  // clear screen
+    LCD_Cmd(0x0F);  // Display on cursor blinking
+             
+    LCD_Data('C');                
+    LCD_Data('O');                    
+    LCD_Data('M');               
+    LCD_Data('O');                                   
+    LCD_Data('R');              
+    LCD_Data('I');              
+    LCD_Data('N');
+      
     while(1); // infinite loop
 }
 
 void LCD_Cmd(unsigned char val)
 {
     P2 = val;
-	  LCD_RS = 0;
-	  LCD_RW = 0;
-	  LCD_EN = 1;
-	  delay();
-	  LCD_EN = 0;
+    LCD_RS = 0;
+    LCD_RW = 0;
+    LCD_EN = 1;
+    delay();
+    LCD_EN = 0;
 }
 void LCD_Data(unsigned char val)
 {
-	  P2 = val;
-	  LCD_RS = 1;
-	  LCD_RW = 0;
-	  LCD_EN = 1;
-	  delay();
-	  LCD_EN = 0;
+      P2 = val;
+      LCD_RS = 1;
+      LCD_RW = 0;
+      LCD_EN = 1;
+      delay();
+      LCD_EN = 0;
 }
 
 void delay()
 {
-	  unsigned int i;
-	  for(i = 0; i <2000; i++);
+      unsigned int i;
+      for(i = 0; i < 2000; i++);
 }
-	  
-	
-	
-	
-	
